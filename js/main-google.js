08bb086338d393e4a9b001a3b687edcbd155f9b3
Giselle <gdgoenaga@gmail.com>
2023-02-16 00:48:49 -0300

    javaScript

---
+++ js/main-google.js
@@ -0,0 +1,26 @@
+// let btn = document.getElementById('btn-chocolate')
+// let chocolate = document.getElementById('chocolate')
+// let input = document.getElementById('input')
+// let historial = document.getElementById('historial')
+
+
+// btn.addEventListener('click', () =>{
+//     chocolate.classList.toggle('active')
+// })
+
+// input.addEventListener('click', () =>{
+//     historial.classList.toggle('active')
+// })
+
+
+{/* <div class="chocolate" id="chocolate">
+<a href="#"><img src="/img/options-google/angout.png" alt=""></a>
+<a href="#"><img src="/img/options-google/calendar.png" alt=""></a>
+<a href="#"><img src="/img/options-google/class_room.png" alt=""></a>
+<a href="#"><img src="/img/options-google/drive.png" alt=""></a>
+<a href="#"><img src="/img/options-google/fotos.png" alt=""></a>
+<a href="#"><img src="/img/options-google/gmail.png" alt=""></a>
+<a href="#"><img src="/img/options-google/maps.png" alt=""></a>
+<a href="#"><img src="/img/options-google/play_store.png" alt=""></a>
+<a href="#"><img src="/img/options-google/translate.png" alt=""></a>
+</div> */}
