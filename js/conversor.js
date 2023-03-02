db885e58cd6a79eeca99e52ab8d9cd57aa611690
Giselle <gdgoenaga@gmail.com>
2023-03-02 16:57:32 -0300

    agregado empty

--- js/conversor.js
+++ js/conversor.js
@@ -1,5 +1,35 @@
+let convert = document.getElementById('convert')
 let cantidad = document.getElementById('cantidad')
 let divisa = document.getElementById('divisa')
 let resultado = document.getElementById('resultado')
-let btn = document.getElementById('btn')
 
+// divisa.addEventListener('change', (e) =>{
+//     console.log(e.target.value)
+// })
+
+// convert.addEventListener('click', () =>{
+//     let elegida = parseInt(divisa.value)
+//     let cuanto = parseInt(cantidad.value)
+//     let result = cuanto*elegida
+//     resultado.innerHTML = `${cuanto} son: ${result}`
+// })
+
+
+let convertilos = () =>{
+    let valor = 0
+    let moneda = "usd"
+
+    if (divisa.value == 'usd'){
+        valor = 370
+        moneda = 'usd'
+    }else{
+        valor = 380
+        moneda = 'eur'
+    }
+    
+    let cuanto = parseInt(cantidad.value)
+    let result = cuanto*valor
+    resultado.innerHTML = `${cuanto} ${moneda} son: ${result} pesos`
+}
+
+convert.addEventListener("click", convertilos)
--- js/main-todo.js
+++ js/main-todo.js
@@ -1,48 +1,69 @@
+// Prueba aparición
+
+let empty = document.getElementById('empty')
+let firstTask = document.getElementById('firstTask')
+let todo = document.getElementById('todo')
+
+//
+
+
 let input = document.querySelector("input");
-let btn = document.querySelector("button");
+let btn = document.getElementById("btn");
 let ul = document.querySelector("ul");
 let mySelect = document.getElementById("my-select");
 let priority = document.getElementById("priority")
 
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
 
-  let task = input.value;
+//Prueba aparición
 
-  if (task !== "") {
-    let taskRepeat = false;
-    let lastTasks = document.querySelectorAll("li p")
-    for (let i = 0; i < lastTasks.length; i++) {
-      if (
-        lastTasks[i].textContent.startsWith(mySelect.value) &&
-        lastTasks[i].textContent.endsWith(`-- ${task} -- ${priority.value}`)
-      ) {
-     taskRepeat = true
-        break;
-      }
-    }
-    if  (taskRepeat) {
-      alert("Ya tenés esta tarea pendiente")
-    } else {
-      let li = document.createElement("li")
-      let p = document.createElement("p")
-      let tarea = `${mySelect.value} -- ${task} -- ${priority.value}`
-      p.textContent = tarea
-      li.appendChild(p)
-      li.appendChild(taskDelete())
-      ul.appendChild(li)
-      input.value = "";
-
-      if(priority.value == "urgente"){
-        p.className = "p-urgente"
-      }else if (priority.value == "postergable"){
-        p.className = "p-postergable"
-      }else{
-        p.className = "p-opcional"
-      }
-    }
-  }
+firstTask.addEventListener('click', () => {
+  todo.style.display = 'block'
+  empty.style.display ='none'
 })
+
+//
+
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+  
+    let task = input.value;
+  
+    if (task !== "") {
+      let taskRepeat = false;
+      let lastTasks = document.querySelectorAll("li p")
+      for (let i = 0; i < lastTasks.length; i++) {
+        if (
+          lastTasks[i].textContent.startsWith(mySelect.value) &&
+          lastTasks[i].textContent.endsWith(`-- ${task} -- ${priority.value}`)
+        ) {
+      taskRepeat = true
+          break;
+        }
+      }
+      if  (taskRepeat) {
+        alert("Ya tenés esta tarea pendiente")
+      } else {
+        let li = document.createElement("li")
+        let p = document.createElement("p")
+        let tarea = `${mySelect.value} -- ${task} -- ${priority.value}`
+        p.textContent = tarea
+        li.appendChild(p)
+        li.appendChild(taskDelete())
+        ul.appendChild(li)
+  
+        input.value = "";
+  
+        if(priority.value == "urgente"){
+          p.className = "p-urgente"
+        }else if (priority.value == "postergable"){
+          p.className = "p-postergable"
+        }else{
+          p.className = "p-opcional"
+        }
+      }
+    }
+  })
+
 
 function taskDelete() {
   let btnDelete = document.createElement("button");
@@ -52,6 +73,13 @@ function taskDelete() {
   btnDelete.addEventListener("click", function (e) {
     let item = e.target.parentElement;
     ul.removeChild(item);
+
+    let items = document.querySelectorAll('li')
+    if (items.length === 0){
+      empty.style.display = 'block'
+      todo.style.display ='none'
+    }
+
   });
 
   return btnDelete;
