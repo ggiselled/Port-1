43865fafb986d6b9ad9c3298ec2cd6c8a94d8db1
Giselle <gdgoenaga@gmail.com>
2023-04-06 22:30:26 -0300

    volver a la pantalla ppal

--- js/main-todo.js
+++ js/main-todo.js
@@ -1,83 +1,75 @@
 
 
-let notask = document.getElementById('notask')
-let yestask = document.getElementById('yestask')
-let taskContainer = document.getElementById('taskcontainer')
-let firstTask = document.getElementById('firstTask')
-let todo = document.getElementById('todo')
+let notask = document.getElementById("notask");
+let taskContainer = document.getElementById("taskContainer");
+let firstTask = document.getElementById("firstTask");
+let todo = document.getElementById("todo");
 let input = document.querySelector("input");
 let btn = document.getElementById("btn");
 let ul = document.querySelector("ul");
 let mySelect = document.getElementById("my-select");
-let priority = document.getElementById("priority")
-let btnBack = document.getElementById('btnBack')
+let priority = document.getElementById("priority");
+
+
+  //ver de meterlo dentro de una función
+  function firstTasks (){
+    firstTask.addEventListener("click", () => {
+      todo.style.display = "block";
+      notask.style.display = "none";
+    });
+
+  }
+  firstTasks()
 
 
 
-firstTask.addEventListener('click', () => {
-  todo.style.display = 'block'
-  notask.style.display ='none'
-  btnBack.style.display ='block'
-})
+  function tareas () {
 
-btnBack.addEventListener('click', () =>{
-  notask.style.display = 'block'
-  todo.style.display = 'none'
-  btnBack.style.display = 'none'
-
-  // let lastTasks = document.querySelectorAll("li p")
-
-  // if (lastTasks){
-  //   notask.style.display = 'none'
-  //   yestask.style.display = 'block'
-  // }else if (taskDelete()){
-  //   notask.style.display = 'block'
-  //   yestask.style.display = 'none'
-  // }
-})
-
-
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
-  
-    let task = input.value;
-  
-    if (task !== "") {
-      let taskRepeat = false;
-      let lastTasks = document.querySelectorAll("li p")
-      for (let i = 0; i < lastTasks.length; i++) {
-        if (
-          lastTasks[i].textContent.startsWith(mySelect.value) &&
-          lastTasks[i].textContent.endsWith(`-- ${task} -- ${priority.value}`)
-        ) {
-      taskRepeat = true
-          break;
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+    
+      let task = input.value;
+    
+      if (task !== "") {
+        let taskRepeat = false;
+        let lastTasks = document.querySelectorAll("li p");
+        for (let i = 0; i < lastTasks.length; i++) {
+          if (
+            lastTasks[i].textContent.startsWith(mySelect.value) &&
+            lastTasks[i].textContent.endsWith(`-- ${task} -- ${priority.value}`)
+          ) {
+            taskRepeat = true;
+            break;
+          }
+        }
+        if (taskRepeat) {
+          alert("Ya tenés esta tarea pendiente");
+        } else {
+          let li = document.createElement("li");
+          let p = document.createElement("p");
+          let tarea = `${mySelect.value} -- ${task} -- ${priority.value}`;
+          p.textContent = tarea;
+          li.appendChild(p);
+          li.appendChild(taskDelete());
+          ul.appendChild(li);
+    
+          input.value = "";
+    
+          if (priority.value == "urgente") {
+            p.className = "p-urgente";
+          } else if (priority.value == "postergable") {
+            p.className = "p-postergable";
+          } else {
+            p.className = "p-opcional";
+          }
         }
       }
-      if  (taskRepeat) {
-        alert("Ya tenés esta tarea pendiente")
-      } else {
-        let li = document.createElement("li")
-        let p = document.createElement("p")
-        let tarea = `${mySelect.value} -- ${task} -- ${priority.value}`
-        p.textContent = tarea
-        li.appendChild(p)
-        li.appendChild(taskDelete())
-        ul.appendChild(li)
-  
-        input.value = "";
+    });
+  }
 
-  
-        if(priority.value == "urgente"){
-          p.className = "p-urgente"
-        }else if (priority.value == "postergable"){
-          p.className = "p-postergable"
-        }else{
-          p.className = "p-opcional"
-        }
-      }
-    }
-  })
+  tareas()
+
+
 
 
 function taskDelete() {
@@ -85,17 +77,28 @@ function taskDelete() {
   btnDelete.textContent = "X";
   btnDelete.className = "taskDelete";
 
+
   btnDelete.addEventListener("click", function (e) {
     let item = e.target.parentElement;
     ul.removeChild(item);
+      if (ul.children.length === 0) {
+        notask.style.display = "block";
+        taskContainer.style.display = "none";
+        todo.style.display = 'none'
+      }
+      taskContainer.style.display = "block";
+      tareas()
 
-    let items = document.querySelectorAll('li')
-    if (items.length === 0){
-      notask.style.display = 'block'
-      todo.style.display ='none'
-    }
+  } );
+  
+  return btnDelete; 
 
-  });
-
-  return btnDelete;
 }
+
+
+
+
+
+
+
+
